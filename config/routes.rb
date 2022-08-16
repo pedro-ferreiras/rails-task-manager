2ac22ddac19fb66fb99c296b9c90e7ebc83a8fec
Pedro Ferreira <pedrom2ferreira@gmail.com>
2022-08-16 15:53:42 +0100

    rails-task-manager

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #verb "path", to: "controller_name#action_name"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: 'new_task'
+  get "tasks/:id", to: "tasks#show", as: 'task'
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to:"tasks#edit", as: 'edit_task'
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: 'delete'
 end
